818b69db75e28668e5f0674d4b1823eefd000bf6
RadhikaYerramsetty <gantamohan.556@gmail.com>
2024-06-27 16:00:32 +0530

    update

--- src/compnonents/Landing/LandingPage.jsx
+++ src/compnonents/Landing/LandingPage.jsx
@@ -6,21 +6,27 @@ import './Lending.css'
 import Services from './Services'
 import Footer from './Footer'
 import AtmCards from './AtmCards'
+import Scroll from './Scroll'
+import Status from './Status'
 
 export default function LandingPage() {
   return (
     <div className='main-page'>
       <NavBar/>
       <Hero/>
+      <Scroll/>
       <div className='About'>
       <About/>
       </div>
+      <Status/>
       <div className='Services'> 
       <Services/>
       </div>
       <div className='Atmcards'>
          <AtmCards/>
       </div>
+      
+      <Scroll/>
       <div className='Footer'>
         <Footer/>
       </div>
